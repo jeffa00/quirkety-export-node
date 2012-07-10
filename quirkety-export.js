f5ea4b5aba08ca7ad3b84d97bb5fe0f51d327790
Jeff Ammons <jeffa00@gmail.com>
2012-07-09 20:13:32 -0400

    Finished first pass that makes most of the site

--- quirkety-export.js
+++ quirkety-export.js
@@ -17,7 +17,9 @@
  * *********************************************** */
 
 var fs = require('fs'); // filesystem library
+var pathLib = require('path'); // path library
 var mu = require('mu2');// Mustache templating library
+var mkd = require('marked'); // Marked markdown library
 var wrench = require('wrench'), // Wrench file library
     util = require('util'); 
 var siteInfo = "";      // JSON file with site specific meta-data
@@ -36,7 +38,7 @@ var sourceDir;          // Base directory where the "magic" happens.
  *     www       <- Output directory suitable for publishing (almost)
  *  ************************************************** */
 
-var processFile = function(path, fileName, level) {
+var processFile = function(basePath, path, fileName, level) {
     /* *************************************************************
      * Recursively traverse through all the directories and files
      * found at the path and process them.
@@ -75,15 +77,62 @@ var processFile = function(path, fileName, level) {
             if (siteInfo.StaticDirectories.indexOf(pathArr[pathArr.length - 1]) > -1){
                 console.log(levelPadding + 'Static Dir');
             }else{
-                processFile(thisFileName + '/', '', level + 1);
+                if (thisFileName.indexOf('/www') === -1) {
+                    var newDirName = thisFileName.replace('site-data','www');
+
+                    fs.mkdirSync(newDirName);
+                    processFile(basePath, thisFileName + '/', '', level + 1);
+                }
             }
         } else {
             if(thisFileName.substr(-5) == '.json') {
                 // If we have a JSON file, we should see if we can match
                 // it with an md or html file, then apply the template
                 console.log(levelPadding + 'Found a json file: ' + thisFileName);
+                
+                var pageMetaData = JSON.parse(fs.readFileSync(thisFileName, 'utf8'));
+
+                var mdFileName = thisFileName.replace('.json','.md');
+                var htmlFileName = thisFileName.replace('.json','.html');
+                var contentsFileName = '';
+                var fileContents;
+                var fileType;
+                //var templateKey = 'default';
+                var outputFileContents;
+
+                if (!pageMetaData.template) {
+                    pageMetaData.template = 'default';
+                }
+
+                if (pathLib.existsSync(mdFileName)) {
+                    contentsFileName = mdFileName;
+                    fileType = 'md';
+                } else if (pathLib.existsSync(htmlFileName)) {
+                    contentsFileName = mdFileName;
+                    fileType = 'html';
+                } 
+                
+                if (contentsFileName != '') {
+                    fileContents = fs.readFileSync(contentsFileName, 'utf8');
+                }
+
+                var thisTemplate = templates[pageMetaData.template];
+                
+                if (fileType === 'md') {
+                    fileContents = mkd(fileContents);
+                }
+
+                pageMetaData.content = fileContents;
+
+                var newFileName = thisFileName.replace('site-data','www').replace('json','html');;
+                //console.log(thisTemplate);
+                var renderStream = mu.renderText(thisTemplate, pageMetaData, {});
+
+                var writeStream = fs.createWriteStream(newFileName);
+                util.pump(renderStream, writeStream);
+
             }else {
-                console.log(levelPadding + 'Found a different file: ' + thisFileName);
+                //console.log(levelPadding + 'Found a different file: ' + thisFileName);
             }
         }
     }
@@ -95,10 +144,10 @@ var loadTemplates = function(path) {
      * have to load them once per run.
      * ************************************************************* */
     var templates = [];
-    var templateFileList= fs.readdirSync(path);
+    var templateFileList= fs.readdirSync(path);  
     for(var i=0; i < templateFileList.length; i++) {
         var fileName = templateFileList[i];
-        if (fileName.indexOf('.html') > 0 && fileName.indexOf('template') > 0)
+        if (fileName.indexOf('.html') > 0 && fileName.indexOf('template') > 0 && fileName.indexOf('~') < 1)
         {
             var keyName = fileName.substring(0, fileName.indexOf('-'));
             var fileContents = fs.readFileSync(path + fileName, 'utf8');
@@ -114,6 +163,23 @@ var moveStaticContent = function(basePath){
 
 var deleteExistingExportDir = function(basePath) {
     wrench.rmdirSyncRecursive(basePath + '/www', true);
+    //fs.rmdirSync(basePath + '/www');
+}
+
+/* Configure marked *********************************
+ * */
+var configureMarked = function() {
+    mkd.setOptions({
+        gfm: true,
+        pedantic: false,
+        sanitize: true,
+        highlight: function(code, lang) {
+            if (lang === 'js') {
+                return javascriptHighlighter(code);
+            }
+            return code;
+        }
+    });
 }
 
 /* Begin processing *********************************
@@ -122,13 +188,14 @@ var deleteExistingExportDir = function(basePath) {
 // If a dir was passed in, use it, else report error and bail.
 if (process.argv[2]) {
     sourceDir = process.argv[2].replace('\\','/');
+    configureMarked();
     siteInfo = JSON.parse(fs.readFileSync(sourceDir + '/site-data/site.json', 'utf8'));
     templates = loadTemplates(sourceDir + '/template/');
     console.log(siteInfo.Title);
     console.log('');
     deleteExistingExportDir(sourceDir);
     moveStaticContent(sourceDir);
-    processFile(sourceDir + '/site-data/', '', lvl);
+    processFile(sourceDir, sourceDir + '/site-data/', '', lvl);
 } else {
     console.log('You must specify a source directory');
     return; 
